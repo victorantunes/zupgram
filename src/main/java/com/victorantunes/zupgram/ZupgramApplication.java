e8cf6d9ddfdbc5054e4068eaa4f0533b7475883b
Victor Antunes <victor.antunes.ignacio@gmail.com>
2020-08-26 19:16:15 -0300

    Initial commit

---
+++ src/main/java/com/victorantunes/zupgram/ZupgramApplication.java
@@ -0,0 +1,13 @@
+package com.victorantunes.zupgram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZupgramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZupgramApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/victorantunes/zupgram/ZupgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victorantunes.zupgram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZupgramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
